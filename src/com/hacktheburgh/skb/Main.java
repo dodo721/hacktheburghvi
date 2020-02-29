e4de80a5a34bdcb1ab74044898b467dc82a8a7b7
dodo721 <dodoworkspr@gmail.com>
2020-02-29 14:12:17 +0000

    Java and LitiEngine skeleton defined

---
+++ src/com/hacktheburgh/skb/Main.java
@@ -0,0 +1,11 @@
+package com.hacktheburgh.skb;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game.init();
+        Game.start( );
+    }
+}
