f2951f68e4df908495ad6224d9499eea4aa34eb0
s-ciciala <s1834237@ed.ac.uk>
2020-02-29 14:56:09 +0000

    didn't do anything rn

--- src/com/hacktheburgh/skb/Main.java
+++ src/com/hacktheburgh/skb/Main.java
@@ -5,7 +5,7 @@ import de.gurkenlabs.litiengine.Game;
 public class Main {
 
     public static void main(String[] args) {
-        Game.init();
-        Game.start( );
+        Game.init();	
+        Game.start(); 	
     }
 }
